1132d760ca0da9e47e679918d78f9d06592da69b
Bandung-Ice <mosesban.rh@gmail.com>
2021-11-03 15:32:36 +0800

    Edit login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,63 +12,59 @@ import Money from '@mui/icons-material/MonetizationOn'
 import Logo from '../assets/logo.svg'
 
 import './Login.css'
-function App() {
+export default function Login() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <div className='Login'>
-          <img src={Logo} alt='logo' />
-          <h4>HR APP</h4>
-          <div className='LoginTextField'>
-            <TextField
-              className='App-TextField'
-              id='Email_TF'
-              label='E-MAIL'
-              variant='filled'
-              type='email'
-              error={false}
-              // helperText='Please enter a valid email address'
-              required
-            />
-            <br />
-            <TextField
-              margin='normal'
-              id='Password_TF'
-              label='Password'
-              variant='filled'
-              type='password'
-              error={false}
-              required
-              // helperText='Incorrect password, please try again'
-            />
-          </div>
-          <div className='Buttons'>
-            <Button
-              fullWidth={true}
-              className='LoginButton'
-              endIcon={<LogInIcon />}
-              variant='contained'
-              color='info'
-              size='small'
-              style={{ fontSize: 22, marginTop: 30 }}
-              href='#'
-            >
-              Log In
-            </Button>
-            <Button
-              variant='text'
-              color='inherit'
-              size='small'
-              style={{ fontSize: 10 }}
-              href='#'
-            >
-              Forgot Password?
-            </Button>
-          </div>
+    <header className='App-header'>
+      <div className='Login'>
+        <img src={Logo} alt='logo' />
+        <h4>HR APP</h4>
+        <div className='LoginTextField'>
+          <TextField
+            className='App-TextField'
+            id='Email_TF'
+            label='E-MAIL'
+            variant='filled'
+            type='email'
+            error={false}
+            // helperText='Please enter a valid email address'
+            required
+          />
+          <br />
+          <TextField
+            margin='normal'
+            id='Password_TF'
+            label='Password'
+            variant='filled'
+            type='password'
+            error={false}
+            required
+            // helperText='Incorrect password, please try again'
+          />
         </div>
-      </header>
-    </div>
+        <div className='Buttons'>
+          <Button
+            fullWidth={true}
+            className='LoginButton'
+            endIcon={<LogInIcon />}
+            variant='contained'
+            color='info'
+            size='small'
+            style={{ fontSize: 22, marginTop: 30 }}
+            href='#'
+          >
+            Log In
+          </Button>
+          <Button
+            variant='text'
+            color='inherit'
+            size='small'
+            style={{ fontSize: 10 }}
+            href='#'
+          >
+            Forgot Password?
+          </Button>
+        </div>
+      </div>
+    </header>
   )
 }
-
-export default App
