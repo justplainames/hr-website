1bcb7cd588c99d01fdd493849d0ce7a2dd7c20e8
Bandung-Ice <mosesban.rh@gmail.com>
2021-11-04 23:58:21 +0800

    croute to App

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Login />
-      {/* <App /> */}
+      {/* <Login /> */}
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
